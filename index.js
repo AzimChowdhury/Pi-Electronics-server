f59c39ea11bb848ab7231ac8a1431c1934cbfd1a
Azim Chowdhury <azimchy994@gmail.com>
2022-04-30 21:30:02 +0600

    database connected and user can addedd

---
+++ index.js
@@ -0,0 +1,51 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const port = process.env.PORT || 5000;
+
+
+//middleware
+app.use(cors());
+app.use(express.json());
+
+
+const uri = "mongodb+srv://pi-electronics:XAFnfB1mplBtGJ1r@cluster0.1lhsr.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run() {
+
+    try {
+        await client.connect();
+        const productsCollection = client.db('pi-electronics').collection('products');
+        app.get('/', async (req, res) => {
+            res.send('server running');
+        })
+
+        app.post('/products', async (req, res) => {
+            const result = await productsCollection.insertOne(req.body)
+            res.send(result)
+        })
+
+        app.get('/products', async (req, res) => {
+            const query = {}
+            const cursor = productsCollection.find(query);
+            const result = await cursor.toArray()
+            console.log(result)
+            res.send(result)
+        })
+    }
+    finally {
+        // await client.close();
+    }
+    app.listen(port, () => {
+        console.log('server running on the port ', port);
+    })
+
+
+}
+
+run().catch(console.dir)
+
+// userName= pi-electronics
+// pass= XAFnfB1mplBtGJ1r
