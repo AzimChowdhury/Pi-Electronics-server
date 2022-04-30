6c72add732fe74a15dba42a6bb11f2860a1fa42d
Azim Chowdhury <azimchy994@gmail.com>
2022-04-30 22:48:28 +0600

    env file and heroku

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const app = express();
+require('dotenv').config()
 const { MongoClient, ServerApiVersion } = require('mongodb');
 const port = process.env.PORT || 5000;
 
@@ -10,7 +11,7 @@ app.use(cors());
 app.use(express.json());
 
 
-const uri = "mongodb+srv://pi-electronics:XAFnfB1mplBtGJ1r@cluster0.1lhsr.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+const uri = `mongodb+srv://${process.env.USER}:${process.env.PASSWORD}@cluster0.1lhsr.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
 async function run() {
