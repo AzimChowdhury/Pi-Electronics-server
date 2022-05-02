af43cf355c5e154313fcb9608bc01b4a13b1b7b7
Azim Chowdhury <azimchy994@gmail.com>
2022-05-02 21:18:56 +0600

    api for single product find by id

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const app = express();
 require('dotenv').config()
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const port = process.env.PORT || 5000;
 
 
@@ -34,6 +34,14 @@ async function run() {
             const result = await cursor.toArray()
             console.log(result)
             res.send(result)
+        })
+
+        app.get('/product/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const product = await productsCollection.findOne(query)
+            console.log(product)
+            res.send(product)
         })
     }
     finally {
